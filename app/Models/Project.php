a3a81d158d5be649c0fd8bb2aa928d8635de278b
PierluigiPapa <pierluigipapa9@gmail.com>
2024-04-02 12:27:38 +0200

    feat: add HasMany and BelongsTo

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 use Illuminate\Support\Str;
 
@@ -21,5 +22,9 @@ class Project extends Model
     public static function generateSlug($title)
     {
         return Str::slug($title, '-');
+    }
+
+    public function type(): BelongsTo {
+        return $this->belongsTo(Type::class);
     }
 }
